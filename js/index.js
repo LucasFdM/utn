eac4f8166ee75194b0f4486175669d324b21af4e
Lucas Ferreyra de Moraiz <ferreyrademoraiz@gmail.com>
2022-02-25 19:33:41 -0300

    creacion de paginas

---
+++ js/index.js
@@ -0,0 +1,60 @@
+/*console.log('soy js');
+
+//Variables
+let nombre; //Declaracion de una variable.
+
+console.log()
+
+let profesion= prompt("hola");
+console.log(profesion);
+
+alert("HOLA");
+
+console.log("...");
+let numeroUno=prompt("introduce numero:");
+let numeroDos=prompt("introduce numero:");
+parseInt(numeroUno);
+parseInt(numeroDos);
+numeroUno=parseFloat(numeroUno);
+var resultado=numeroUno+numeroDos;
+console.log("el resultado es " +resultado);*/
+
+//Definir variables
+//let num1, num2, r=0;
+
+//Funciones
+/*function iniciar(){
+	num1=parseFloat(prompt("introduzca el 1° numero: "));
+	num2=parseFloat(prompt("introduzca el 2° numero: "));
+}
+
+function sum(r){
+	r=num1+num2;
+}
+
+function res(r){
+	r=num1-num2;
+}
+
+function mul(r){
+	r=num1*num2;
+}
+
+function div(r){
+	r=num1/num2;
+}
+
+let resultado = document.getElementById("resultado");
+resultado.innerHTML="El resultado es: " + r;
+*/
+
+console.log('Iniciando Login');
+
+let usuario="administrador", password="1234";
+
+function Login(){
+	usuario=document.getElementById('usuario').value;
+	console.log(usuario);
+
+	
+}
